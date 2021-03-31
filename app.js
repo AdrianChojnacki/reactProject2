8dcece5c2b6788a0e9a895227bc2c824dfffa954
Adrian Chojnacki <chojnackiadrian1@gmail.com>
2021-03-31 16:31:23 +0200

    my version

--- app.js
+++ app.js
@@ -1,11 +1,57 @@
 class App extends React.Component {
-  state = {};
+  state = {
+    availableProducts: 7,
+    shoppingCart: 0,
+    isMinusDisabled: true,
+    isPlusDisabled: false,
+  };
+
+  handleMinusClick = () => {
+    if (this.state.isPlusDisabled) {
+      this.setState({
+        isPlusDisabled: false,
+      });
+    }
+
+    if (this.state.shoppingCart <= 1) {
+      this.setState({
+        isMinusDisabled: true,
+      });
+    }
+
+    this.setState({
+      shoppingCart: this.state.shoppingCart - 1,
+    });
+  };
+
+  handlePlusClick = () => {
+    if (this.state.isMinusDisabled) {
+      this.setState({
+        isMinusDisabled: false,
+      });
+    }
+
+    if (this.state.shoppingCart >= this.state.availableProducts - 1) {
+      this.setState({
+        isPlusDisabled: true,
+      });
+    }
+
+    this.setState({
+      shoppingCart: this.state.shoppingCart + 1,
+    });
+  };
+
   render() {
     return (
       <React.Fragment>
-        <button>-</button>
-        <span> 0 </span>
-        <button>+</button>
+        <button onClick={this.handleMinusClick} disabled={this.state.isMinusDisabled}>
+          -
+        </button>
+        <span> {this.state.shoppingCart} </span>
+        <button onClick={this.handlePlusClick} disabled={this.state.isPlusDisabled}>
+          +
+        </button>
       </React.Fragment>
     );
   }
